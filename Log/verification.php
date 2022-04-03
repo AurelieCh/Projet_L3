84e6ef11f03f26d7ef08fe66e764a454b30a117f
Siegroud <siegrid@outlook.fr>
2022-04-03 10:57:52 +0200

    modifs connexion 1

--- Log/verification.php
+++ Log/verification.php
@@ -1,11 +1,11 @@
 <?php
    session_start();
-   if(isset($_POST['email']) && isset($_POST['motdepasse'])) {
+   if(isset($_POST['email']) && isset($_POST['password'])) {
 
       // connexion à la base de données
       $db_username = 'root';
-      $db_password = '3243';
-      $db_name     = 'Base_de_Donnees_NouveauRegard';
+      $db_password = '';
+      $db_name     = 'nouveau_regard';
       $db_host     = 'localhost';
 
       $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
@@ -13,15 +13,15 @@
 
       // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars pour éliminer toute attaque de type injection SQL et XSS
       $email = mysqli_real_escape_string($db,htmlspecialchars($_POST['email'])); 
-      $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['motdepasse']));
+      $password = mysqli_real_escape_string($db,htmlspecialchars($_POST['password']));
 
       if($email !== "" && $password !== "") {
-          $requete = "SELECT count(*) FROM Utilisateur where 
-                nom_utilisateur = '".$email."' and mot_de_passe = '".$password."' ";
+          $requete = "SELECT count(*) FROM user where 
+                email = '".$email."' and password = '".$password."' ";
           $exec_requete = mysqli_query($db,$requete);
           $reponse = mysqli_fetch_array($exec_requete);
           $count = $reponse['count(*)'];
-          if($count!=0) { // nom d'utilisateur et mot de passe correctes
+          if($count!=0) { // nom d'utilisateur et mot de passe corrects
              $_SESSION['email'] = $email;
              header('Location: /index2.html');
           }
